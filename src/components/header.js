15a0675cb08676c3804192e95cdd6bf215ed9d90
Eskabore <turquinjl@gmail.com>
2021-02-22 04:56:00 +0100

    Started Navbar design

--- src/components/header.js
+++ src/components/header.js
@@ -3,31 +3,23 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        minHeight: `8vh`,
-	backgroundColor: `rgba(31, 30, 30, 0.24)`,
-	transition: `.3s ease background-color`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+<div id="header" className="container">
+      <div className="nav-bar">
+        <div className="brand">
+          <a href="#hero"><h1><span>S</span>haif <span>A</span>rfan</h1></a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger"><div className="bar"></div></div>
+          <ul>
+            <li><a href="#hero" data-after="Home">Home</a></li>
+            <li><a href="#services" data-after="Service">Services</a></li>
+            <li><a href="#projects" data-after="Projects">Projects</a></li>
+            <li><a href="#about" data-after="About">About</a></li>
+            <li><a href="#contact" data-after="Contact">Contact</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </header>
 )
